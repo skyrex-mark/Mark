b961c162aea32b76e500fec0484afc0207ea8cea
Mark Xie <xiaofeng02111@gmail.com>
2018-09-27 01:17:12 -0400

    Updated yarn.lock

---
+++ src/component/Splash/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Header, Image, Segment } from "semantic-ui-react";
+const Navigation = () => (
+  <Segment basic>
+    <Image />
+    <Header raised as="h1" padded="very" size="large" textAlign="center">
+      Xiaofeng Xie
+    </Header>
+    <Header
+      secondary
+      as="h2"
+      padded="very"
+      textAlign="center"
+      inverted
+      color="grey"
+    >
+      Read , Code and Think
+    </Header>
+  </Segment>
+);
+
+export default Navigation;
