24111299bfaafc09742c29d35460654b22ce452b
Mark Xie <xiaofeng02111@gmail.com>
2018-09-27 01:22:20 -0400

    Updated yarn.lock (#7)

---
+++ src/page/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+import Splash from "../../component/Splash";
+
+const Home = () => (
+  <Container>
+    <Splash />
+  </Container>
+);
+
+export default Home;
