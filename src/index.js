dc4a28c8a38b7853f600fe0b1641bf62b867052a
Mark Xie <xiaofeng02111@gmail.com>
2018-09-29 00:59:35 -0400

    Added air brake

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
-
+const airbrake = new airbrakeJs.Client({
+  projectId: 197469,
+  projectKey: "c8a4a821b675324b8ea6a7b7e65098b4"
+});
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
