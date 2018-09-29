00e2b18d6cc3e00730bb144768146af0a26162d3
Mark Xie <xiaofeng02111@gmail.com>
2018-09-29 14:40:07 -0400

    Added Google analytics module (#16)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
 import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
+
+ReactGA.initialize("UA-126648076-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
