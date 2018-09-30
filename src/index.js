f407a375ca5a07c455fb5bf80c8c14781d7ba166
Mark Xie <xiaofeng02111@gmail.com>
2018-09-30 01:51:17 -0400

    Add log rocket (#20)
    * Added LogRocket
    
    * Removed GA

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
+import LogRocket from "logrocket";
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactGA from "react-ga";
 import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactGA.initialize("UA-126648076-1");
-ReactGA.pageview(window.location.pathname + window.location.search);
+LogRocket.init("qlt1rm/mark");
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
