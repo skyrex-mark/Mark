b923b660a55562624a8fa530aaa749b926aa5a36
Mark Xie <xiaofeng02111@gmail.com>
2018-09-30 13:39:57 -0400

    Removed docz cache

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  "src/App.mdx": () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-app" */ "src/App.mdx"),
-  "src/component/Splash/Splash.mdx": () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-component-splash-splash" */ "src/component/Splash/Splash.mdx")
-};
