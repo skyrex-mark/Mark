c3f1ca2cba78d2a72e62e97128c54070c478f607
Mark Xie <xiaofeng02111@gmail.com>
2018-09-30 21:08:39 -0400

    Added splash screen (#27)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import logo from "./logo.svg";
-
+import ColorSegment from "./component/ColorSegment";
+import Splash from "./component/Splash";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <>
+        <ColorSegment />
+        <Splash />
+      </>
     );
   }
 }
---
+++ src/component/ColorSegment/style.js
@@ -0,0 +1,11 @@
+export default {
+  canvas: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    zIndex: 0,
+    width: "100%",
+    height: "100%",
+    pointerEvents: "none"
+  }
+};
